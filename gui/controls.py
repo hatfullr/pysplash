Fixed units

gui/controls.py
@@ -47,6 +47,8 @@ class Controls(tk.Frame,object):
         
         for variable in self.get_variables():
             variable.trace('w',self.on_state_change)
+
+        self.gui.bind("<<PlotUpdate>>",self.on_plot_update,add="+")
         
         self.saved_state = None
         self.previous_state = None
@@ -203,15 +205,13 @@ class Controls(tk.Frame,object):
             self.gui.read()
             need_reset = True
 
-        # If the x, y, or colorbar axes' combobox values
+        # If the x, y, or colorbar axes' combobox/entry values changed
         for axis_controller in self.axis_controllers.values():
             if axis_controller.value in changed_variables:
                 need_reset = True
                 break
 
-        # Note: changing the units also changes the associated axis controls limits
-            
-        # Check if the user changed any of the x or y axis limits
+        # Check if the user changed any of the x or y axis limits (changing units also changes limits)
         if self.is_limits_changed(('XAxis','YAxis','Colorbar')):
             # Cancel any queued zoom
             if self.is_limits_changed(('XAxis','YAxis')):
@@ -220,7 +220,7 @@ class Controls(tk.Frame,object):
             user_xlims = self.axis_controllers['XAxis'].limits.get()
             user_ylims = self.axis_controllers['YAxis'].limits.get()
             user_clims = self.axis_controllers['Colorbar'].limits.get()
-
+            
             if np.isnan(user_xlims[0]): user_xlims = (None, user_xlims[1])
             if np.isnan(user_xlims[1]): user_xlims = (user_xlims[0], None)
             if np.isnan(user_ylims[0]): user_ylims = (None, user_ylims[1])
@@ -285,3 +285,14 @@ class Controls(tk.Frame,object):
                 result.append(item[0])
         
         return result
+
+
+    def on_plot_update(self, *args, **kwargs):
+        if globals.debug > 1: print("controls.on_plot_update")
+
+        # Store the axis limits
+        self.previous_xaxis_limits = self.gui.interactiveplot.ax.get_xlim()
+        self.previous_yaxis_limits = self.gui.interactiveplot.ax.get_ylim()
+        self.previous_caxis_limits = self.gui.interactiveplot.colorbar.get_cax_limits()
+
+        self.save_state()

gui/gui.py
@@ -304,24 +304,27 @@ class GUI(tk.Frame,object):
         if globals.debug > 1: print("gui.get_physical_units")
         return self.data['physical_units'][key]
 
-    def get_display_data(self,key,scaled=True):
+    def get_display_data(self,key,raw=False):
         if globals.debug > 1: print("gui.get_display_data")
-        #d = self.get_data(key)*self.get_display_units(key)
-        d = self.display_data[key]
-        if scaled:
+        if raw: return self.display_data[key]
+        else: # Apply the axis scale and units to the data
             # Check to see if the key matches
             xaxis = self.controls.axis_controllers['XAxis'].value.get()
             yaxis = self.controls.axis_controllers['YAxis'].value.get()
-            controller = None
             if key == xaxis:
                 controller = self.controls.axis_controllers['XAxis']
             elif key == yaxis:
                 controller = self.controls.axis_controllers['YAxis']
-            if controller:
-                scale = controller.scale.get()
-                if scale == 'log10': d = np.log10(d)
-                elif scale == '^10': d = 10**d
-        return d
+            else:
+                raise RuntimeError("None of the AxisControllers have the value '"+xaxis+"' or '"+yaxis+"'")
+            units = controller.units.value.get()
+            previous_units = controller.previous_units
+            scale = controller.scale.get()
+            if scale == 'linear': return self.display_data[key]/units
+            elif scale == 'log10': return np.log10(self.display_data[key]/units)
+            elif scale == '^10': return 10**(self.display_data[key]/units)
+            else:
+                raise RuntimeError("The AxisController '"+controller+"' has scale '"+scale+"', which is not one of 'linear', 'log10', or '^10'")
     
     def next_file(self,*args,**kwargs):
         if globals.debug > 1: print("gui.next_file")

widgets/axiscontroller.py
@@ -49,9 +49,13 @@ class AxisController(LabelledFrame,object):
         self.limits.high.trace('w',self.update_scale_buttons)
         #self.units.value.trace('w',self.update_limits)
 
+        self.gui.bind("<<PlotUpdate>>", self.update_scale_buttons, add="+")
+        
         if self.usecombobox:
             self.combobox.bind("<<ComboboxSelected>>", self.on_combobox_selected, add='+')
+            
         self.previous_value = None
+
         
 
     def get_variables(self,*args,**kwargs):
@@ -117,7 +121,7 @@ class AxisController(LabelledFrame,object):
         # the user is in log10 scale mode, we need to switch them out of it
         if self.usecombobox:
             if self.value.get() in self.gui.data['data'].keys():
-                if any(self.gui.get_display_data(self.value.get(), scaled=False) <= 0):
+                if any(self.gui.get_display_data(self.value.get(), raw=True) <= 0):
                     if self.scale.get() == "log10":
                         self.scale.linear_button.invoke()
                         self.on_scale_changed()
@@ -169,7 +173,7 @@ class AxisController(LabelledFrame,object):
         #if self.gui.data is not None:
         #    if self.value.get() not in self.gui.data['data'].keys(): return
         #
-        #    data = self.gui.get_display_data(self.value.get(), scaled=False)
+        #    data = self.gui.get_display_data(self.value.get(), raw=True)
         #    if np.any(data <= 0):
         #        self.scale.log_button.configure(state='disabled')
         #        return
@@ -244,7 +248,6 @@ class AxisController(LabelledFrame,object):
                 limit.set(limit.get() * self.previous_units / units)
             self.previous_units = units
 
-
     def set_adaptive_limits(self, *args, **kwargs):
         if globals.debug > 1: print("axiscontroller.set_adaptive_limits")
         # Set the limit entries to be the data's true, total limits
@@ -263,3 +266,4 @@ class AxisController(LabelledFrame,object):
                 if self.scale.get() == 'log10': newlim = np.log10(newlim)
                 elif self.scale.get() == '^10': newlim = 10.**newlim
                 self.limits.set_limits(newlim)
+        