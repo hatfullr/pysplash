Fixed a problem with units when using arbitrary plotting code

widgets/mathentry.py
@@ -84,8 +84,8 @@ class MathEntry(FlashingEntry, object):
         data = self.gui.data['data']
         
         variables = {key:self.gui.get_display_data(key,identifier=self.get(),scaled=False) for key in data.keys()}
-        physical_units = {key:self.gui.get_physical_units(key) for key in data.keys()}
-        display_units = {key:self.gui.get_display_units(key) for key in data.keys()}
+        physical_units = {key:np.array([self.gui.get_physical_units(key)]) for key in data.keys()}
+        display_units = {key:np.array([self.gui.get_display_units(key)]) for key in data.keys()}
 
         return variables, physical_units, display_units
     