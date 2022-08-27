66b2a728f32daa117490fbebfa4aab0bfb0dfcca
hatfullr <hatfull@ualberta.ca>
2022-08-26 20:00:16 -0600

    Added GPU support for point density plots

--- lib/pointdensityplot.py
+++ lib/pointdensityplot.py
@@ -23,15 +23,10 @@ class PointDensityPlot(ScatterPlot, object):
         y = y[valid]
 
         self.unique_x = np.unique(x)
-        self.calculated = False
         self.xbins = None
         self.ybins = None
         
         super(PointDensityPlot,self).__init__(ax,x,y,s=s,**kwargs)
-        
-    if has_jit:
-        def calculate_data_gpu(self,x,y,c):
-            self.calculate_data_cpu(x,y,c)
 
     def calculate_xypixels(self,*args,**kwargs):
         if globals.debug > 1: print("pointdensityplot.calculate_xypixels")
@@ -55,23 +50,67 @@ class PointDensityPlot(ScatterPlot, object):
                 self.xbins[-1] = self.unique_x[-1]
                 self.xbins[1:-1] = self.unique_x[:-1] + 0.5*dx
 
-    def calculate(self,*args,**kwargs):
-        if globals.debug > 1: print("pointdensityplot.calculate")
-        if not self.calculated:
-            super(PointDensityPlot,self).calculate(*args,**kwargs)
-        self.calculated = True
+    if has_jit:
+        @staticmethod
+        @cuda.jit('void(float32[:,:], float64[:], float64[:], float32, float32, float32, float32)')
+        def calculate_gpu(data, y, x, ymin, xmin, invdy, invdx):
+            i = cuda.grid(1)
+            if i < y.shape[0]:
+                cuda.atomic.add(
+                    data,
+                    (
+                        int((y[i] - ymin)*invdy), # y index
+                        int((x[i] - xmin)*invdx), # x index
+                    ),
+                    1 # Add 1
+                )
+        
+        @profile
+        def calculate_data_gpu(self,x,y,c):
+            N = len(x)
+            
+            dx = (self._extent[1]-self._extent[0])/float(self.xbins)
+            dy = (self._extent[3]-self._extent[2])/float(self.ybins)
+
+            device_y = cuda.to_device(y)
+            device_x = cuda.to_device(x)
+            
+            device_data = cuda.to_device(self._data.astype('float32'))
+            
+            blockspergrid = N // globals.threadsperblock + 1
+            self.calculate_gpu[blockspergrid, globals.threadsperblock](
+                device_data,
+                device_y,
+                device_x,
+                self._extent[2]-0.5*dy,
+                self._extent[0]-0.5*dx,
+                1./dy,
+                1./dx,
+            )
+            cuda.synchronize()
+            data = device_data.copy_to_host()
+            data /= np.amax(data)
+            data[data == 0] = np.nan
+            self._data = data
 
     def calculate_data_cpu(self,x,y,c):
         if globals.debug > 1: print("pointdensityplot.calculate_data_cpu")
         pixels,yedges,xedges = np.histogram2d(
-            self.y,self.x,
+            y,x,
             bins=[self.ybins,self.xbins],
         )
-        self._extent = [np.nanmin(xedges),np.nanmax(xedges),np.nanmin(yedges),np.nanmax(yedges)]
-        
-        pixels[pixels == 0] = np.nan
+        # This step ensures that the bins appear centered
+        dx = xedges[1]-xedges[0]
+        dy = yedges[1]-yedges[0]
+        self._extent = [
+            np.amin(xedges) - 0.5*dx, # xmin
+            np.amax(xedges) - 0.5*dx, # xmax
+            np.amin(yedges) - 0.5*dy, # ymin
+            np.amax(yedges) - 0.5*dy, # ymax
+        ]
 
-        pixels /= np.nanmax(pixels)
+        pixels /= np.amax(pixles)
+        pixels[pixels == 0] = np.nan
         
         self._data = pixels
 
